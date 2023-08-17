35139c3c684a67b2322981119080abbf0704212f
Thiago Barcellos <111370569+ThiagoEmp@users.noreply.github.com>
2023-08-17 14:38:46 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,22 @@
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
-
 const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".botao-lua")
 
-botaoAlterarTema.addEventListener("click", () =>{
-    body.classList.add("modo-claro");
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoClaroEstaAtivo = body.classList.contains("modo-claro");
+
+    if (modoClaroEstaAtivo) {
+        body.classList.remove("modo-claro");
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/logo-lua.png")
+    } else {
+        body.classList.add("modo-claro")
+
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/logo-sol.png")
+    }
+
+
+
+
 });
